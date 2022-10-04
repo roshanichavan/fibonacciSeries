cf5d9e2dea9d5906503251fcff1ac35d527fe263
roshani chavan <roshanic355@gmail.com>
2022-10-04 21:00:35 +0530

    ReverseNumber

---
+++ ReverseNumber.java
@@ -0,0 +1,25 @@
+package Logical_Programs;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+	public static void main(String[] args) {
+	System.out.println("--------------Reverse A Number-----------------"); 
+	int reverse=0;
+	
+	Scanner sc= new Scanner(System.in); 
+	System.out.print("Enter Value Number : ");
+	int Num= sc.nextInt();  
+	while(Num != 0)   
+	{  
+	int remainder = Num % 10;
+	
+	reverse = reverse * 10 + remainder;  
+	Num = Num/10;  
+	}  
+	System.out.println("The reverse of the given number is: " + reverse);  
+	}  
+	
+	}
+
+
