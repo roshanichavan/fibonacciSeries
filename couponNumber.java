b51a41c76ffd555e0b039200b6de01e44edeaefb
roshani chavan <roshanic355@gmail.com>
2022-10-07 14:32:31 +0530

    Added:couponNumber

---
+++ couponNumber.java
@@ -0,0 +1,23 @@
+package Logical_Programs;
+
+public class couponNumber {
+	public static void main(String[] args) 
+	{
+		System.out.println("--------------Coupon Number-----------------");
+		char[] chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789".toCharArray();
+		int max=100000000;
+		int random=(int) (Math.random()*max);	
+		StringBuffer sb=new StringBuffer();
+		
+		while (random>0)
+		{
+			sb.append(chars[random % chars.length]);
+			random /= chars.length;
+		}
+
+		String couponCode=sb.toString();
+		System.out.println("Coupon Code: "+couponCode);	
+	}
+
+}
+
